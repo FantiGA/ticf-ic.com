9d6f76bc9afef4081b0a163b1d8bb008d0ef3317
wangb <870719738@qq.com>
2025-02-21 11:39:56 +0900

    refactor: rebuild Header and Footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <>
+            <div className="bottom">
+                <p style="font-size: 10px;">Copyright(c) 2019 TICF All Rights Reserved.</p>
+            </div>
+        </>
+
+
+    );
+}
+export default Footer;
---
+++ components/Header/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+const Header = () => {
+    return (
+        <>
+            {/* 导航 */}
+            <div className="drop visible opacity withBG bgw">
+                <div className="option active placeholder" data-value="placeholder">
+                    中文简体
+                </div>
+                <div className="option option_topnone" data-value="wow">
+                    <a href="index2.html">日本語</a>
+                </div>
+                <div className="option option_topnone" data-value="wow">
+                    <a href="index3.html">English</a>
+                </div>
+
+            </div>
+
+            <div className="nav bgw" id="nav">
+                <div className="n-left">
+                    <img className="top_logo" src="img/top_logo.png" />
+                </div>
+
+                <div className="n-r">
+                    <div className="three col">
+                        <div className="hamburger" id="hamburger-1">
+                            <span className="line"></span>
+                            <span className="line"></span>
+                            <span className="line"></span>
+                        </div>
+                        <p className="nav-text">MENU</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            {/* 菜单显示\隐藏 */}
+            <div className="cd animated">
+                <ul>
+                    <li id="top">我们的使命</li>
+                    <li id="about">我们的异象</li>
+                    <li id="menu">我们的核心价值观</li>
+                    <li id="statement">我们的信仰声明</li>
+                    <li id="access">礼拝信息</li>
+                    <li id="contact">联系方式</li>
+                </ul>
+
+            </div>
+
+            {/* button TOP */}
+            <div className="nav-top">
+                <img src="img/top.png" />
+            </div>
+        </>
+    );
+}
+
+export default Header;
